55d86eb85e7a36f79731e286c39a4ad5703883c6
bhagyashreeWalanj <shree.6490@gmail.com>
2025-06-24 12:55:45 +0200

    added new supabase db details

--- src/api/authprovider.ts
+++ src/api/authprovider.ts
@@ -5,6 +5,6 @@ const supabaseKey: any = process.env.REACT_APP_SUPABASE_ANON_KEY;
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
 
 export const CDNURL =
-  "https://rixfolsvairsbvmikclq.supabase.co/storage/v1/object/public/images/";
+  supabaseUrl+"/storage/v1/object/public/images/";
 
 export default supabase;
